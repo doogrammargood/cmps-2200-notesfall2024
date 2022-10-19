1594721fedc093bca6d3b62dc45d598aa6d557a0
Aaron Maus <arobie2@gmail.com>
2022-10-19 10:23:58 -0500

    Update Notebooks

--- scripts/make_schedule.py
+++ scripts/make_schedule.py
@@ -1,6 +1,7 @@
 import glob
 import re
-
+github_organization = "CMPS-2200"
+github_repo = "cmps-2200-slides"
 def get_header(fname):
 	try:
 		return re.sub('#', '', open(fname).readlines()[0]).strip()
@@ -9,19 +10,19 @@ def get_header(fname):
 
 for module in sorted(glob.glob("module*")):
 	module_title = '**%s**' % get_header('%s/README.md' % module)
-	print('|[%50s](https://github.com/tulane-cmps2200/slides/tree/master/%s)|' % (module_title, module))
+	print('|[%50s](https://github.com/%s/%s/tree/main/%s)|' % (module_title, github_organization, github_repo, module))
 	for lecture in sorted(glob.glob('%s/0*' % module)):
 		lecture_title = '&nbsp;&nbsp;%s' % get_header('%s/README.md' % (lecture))
 		ipynb = 'n/a'
 		try:
 			ipynb = glob.glob('%s/*ipynb' % lecture)[0]
-			ipynb = '[static](https://nbviewer.jupyter.org/github/tulane-cmps2200/slides/blob/master/%s?flush_cache=True)' % ipynb
+			ipynb = '[static](https://nbviewer.jupyter.org/github/%s/%s/blob/main/%s?flush_cache=True)' % (github_organization, github_repo, ipynb)
 		except:
 			pass
 		live = ' '
 		try:
 			live = glob.glob('%s/*ipynb' % lecture)[0]
-			live = '[live](https://mybinder.org/v2/gh/tulane-cmps2200/slides/master?filepath=%s)' % live
+			live = '[live](https://mybinder.org/v2/gh/%s/%s/main?filepath=%s)' % (github_organization, github_repo, live)
 		except:
 			pass
 		# pdf = ' '
